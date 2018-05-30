8ea60e8d9d153a95cdce26bf9f454c29b9cd7671
anilpython <anilpython1@gmail.com>
2018-05-30 22:56:48 +0530

    Python Exceptions

---
+++ Python_Exceptions.py
@@ -0,0 +1,23 @@
+str="anil kumar"
+l=len(str)
+try:
+    for single_char in l-1:
+        print('hi')
+except(TypeError):
+    print("int object is not iterable")
+##############################################
+str="anil kumar"
+l=len(str)
+try:
+    for read_single_char in range(l+1):
+        print(str[read_single_char])
+except(IndexError):
+    print("string index out of range")
+##############################################
+str="anil kumar"
+l=len(str)
+try:
+    for read_single_char in range(l):
+        print(str[read_single_char])
+except(IndentationError):
+    print("string index out of range")
